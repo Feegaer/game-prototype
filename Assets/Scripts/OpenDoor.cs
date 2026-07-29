8a687ef64e531e577417f6e301ae34ef992ccdfb
Feegaer <lanevecristianezequiel@gmail.com>
2026-07-28 22:53:24 -0300

    Added interactable door, now you can open/close doors

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,28 @@
+﻿using Assets.Scripts.Interfaces;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class OpenDoor : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private float openAngle = 90f;
+        [SerializeField] private float openSpeed = 2f;
+        private bool isOpen = false;
+        public void Interact()
+        {
+            isOpen = !isOpen;
+
+            if(isOpen)
+            {
+                // Open the door
+                transform.Rotate(0, 0, openAngle, Space.Self);
+            }
+            else
+            {
+                // Close the door
+                transform.Rotate(0, 0, -openAngle, Space.Self);
+            }
+        }
+        public string InteractionPrompt => "Press [E] to open door.";
+    }
+}
